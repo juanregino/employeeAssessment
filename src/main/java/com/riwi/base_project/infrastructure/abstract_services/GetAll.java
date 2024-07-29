604ae8659f70e2fff44feeefe6c14e923d237ec4
MiguelAngelToro <rengell.angell@gmail.com>
2024-07-29 12:43:39 -0500

    getALL was update

--- src/main/java/com/riwi/base_project/infrastructure/abstract_services/GetAll.java
+++ src/main/java/com/riwi/base_project/infrastructure/abstract_services/GetAll.java
@@ -3,5 +3,5 @@ package com.riwi.base_project.infrastructure.abstract_services;
 import java.util.List;
 
 public interface GetAll<RequestDTO,ResponseDTO, ID> {
-    List<ResponseDTO> getAll(ID id);
+    List<ResponseDTO> getAll();
 }
