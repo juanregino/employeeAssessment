cc2b5416f33f948c94b01cc42a56c4606665ff5e
caritogcdev <carito940905@gmail.com>
2024-07-29 08:48:33 -0500

    first commit

---
+++ src/main/java/com/riwi/base_project/BaseProjectApplication.java
@@ -0,0 +1,15 @@
+package com.riwi.base_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseProjectApplication.class, args);
+
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/test/java/com/riwi/base_project/BaseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.base_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
